518c4a52991466e150b5405ede7ac1a274e9992e
opilane <opilane@HP-CZC3217068>
2025-02-04 09:34:10 +0200

    shithubdesktop

--- htdocs/TARpe23_Marko_Maripuu/2p/12/12.js
+++
@@ -1,8 +0,0 @@
-var pealkiri1 = document.querySelector("h1");
-let html = "<h1>Ostukorv</h1>";
-pealkiri1.insertAdjacentHTML("afterend", html);
-
-const newlist = document.createElement('ul');
-
-newlist.setAttribute("id", "ostukorv");
-newlist.textContent = '<li>Uus lõik alguses</li>';
--- htdocs/locomotive/.htswvwfd.appconfig.php
+++
@@ -1,2 +0,0 @@
-<?php header("HTTP/1.0 404 Not Found");exit;?>
-pid=tN7qUOkE3jslkeGkIF1vnOeFdDEk/Fb7IEgFpxO1YYg=
